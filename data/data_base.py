26e9e19f2b8072745e4e00d78be2506964d18822
Artem Vysogorets <avysogorets@10-19-135-169.dynapool.wireless.nyu.edu>
2024-01-21 15:46:25 -0500

    initial commit

--- data/data_base.py
+++ data/data_base.py
@@ -1,13 +1,9 @@
 from torch.utils.data import Dataset
 from typing import Dict
-import torch
 
 
 class ClassificationDataBase:
 
-    def __init__(self,
-                path: str,
-                device: torch.device,
-                **kwargs):
+    def __init__(self, **kwargs):
         self.num_classes: int
         self.datasets: Dict[str, Dataset] = {'train': None, 'dev': None}
